a9db1d6783503ba1354888841769ed9545bde08a
Varenya Tiwari <varenya.tiwari@iitg.ac.in>
2022-07-14 11:51:32 +0530

    React Conifg Done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "cordova_script";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+document.addEventListener(
+  "deviceready",
+  () => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <div>
+          <App cordovaWork={true} />
+        </div>
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+  },
+  false
 );
 
 // If you want to start measuring performance in your app, pass a function
